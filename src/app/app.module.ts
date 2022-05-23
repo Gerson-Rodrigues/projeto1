13297d9c856450e68b3b5f8c9bd014553f9672fe
Gerson Rodrigues <rodrigues87gerson@gmail.com>
2022-05-23 15:03:49 -0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelPrincipalComponent } from './painel-principal/painel-principal.component';
+import { CadastroTarefasComponent } from './cadastro-tarefas/cadastro-tarefas.component';
+import { ConsultaTarefasComponent } from './consulta-tarefas/consulta-tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelPrincipalComponent,
+    CadastroTarefasComponent,
+    ConsultaTarefasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
