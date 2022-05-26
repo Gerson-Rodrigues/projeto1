1908cb6dcbadfc7518620a91a032bcaa36bd95ac
Gerson Rodrigues <rodrigues87gerson@gmail.com>
2022-05-26 01:12:22 -0300

    atualização

--- src/app/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -8,41 +8,45 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./cadastro-tarefas.component.css']
 })
 export class CadastroTarefasComponent implements OnInit {
-
+  //Atributos (campos)
+  message = "";
   //método Construtor--> incializar os serviços di componente 
   constructor() { }
-//função executa antes do componente abrir na página
+  //função executa antes do componente abrir na página
   ngOnInit(): void {
   }
 
-formCadastroTarefa = new FormGroup({
-  //Capaturando cada campo do formulário 
-  //nome da tarefa (campo do fomulário)
-  nome: new FormControl('',[
-    Validators.required, //campo Obrigatório
-    Validators.minLength(6),
-    Validators.maxLength(150),
-    
-  ]),
-  data: new FormControl('',[]),
-  hora: new FormControl('',[]),
-  descricao: new FormControl('',[]),
-  prioridade: new FormControl('',[]),
-});
-//Criando um objeto para que seja possivel utilizar 
-//o FormControl (formCadastroTarefa) na página HTML
+  formCadastroTarefa = new FormGroup({
+    //Capaturando cada campo do formulário 
+    //nome da tarefa (campo do fomulário)
+    nome: new FormControl('', [
+      Validators.required, //campo Obrigatório
+      Validators.minLength(6), //minimo de caracteres
+      Validators.maxLength(150),// máx de caracteres
+    ]),
+    data: new FormControl('', [Validators.required,]),
+    hora: new FormControl('', [Validators.required,]),
+    descricao: new FormControl('', [Validators.required, Validators.maxLength(300),]),
+    prioridade: new FormControl('', [Validators.required,]),
+  });
+  //Criando um objeto para que seja possivel utilizar 
+  //o FormControl (formCadastroTarefa) na página HTML
 
-get form(): any{
-  //permitir que possamos acessar o conteudo
-  //do objeto 'formCadastroTarefa' na página HTML
-  return this.formCadastroTarefa.controls;
-}
+  get form(): any {
+    //permitir que possamos acessar o conteudo
+    //do objeto 'formCadastroTarefa' na página HTML
+    return this.formCadastroTarefa.controls;
+  }
 
-//função para capturar o evento SUBMIT do formulario
-onSubmit(): void{
-  //Imprimir o conteudo do formulario
-  console.log(this.formCadastroTarefa.value);
-}
+  //função para capturar o evento SUBMIT do formulario
+  onSubmit(): void {
+    //Imprimir o conteudo do formulario
+    console.log(this.formCadastroTarefa.value);
+  }
+  //função para limpar menssagem
+  clearMessage() {
+    this.message = "";
+  }
 
 }//fecha o cadastro-tarefas.component.ts
 //angularforms
